dcc0b7645db01d0c638a1e741d641742e8bfb7fd
SamCocho127 <samir.campos.diaz@est.una.ac.cr>
2026-04-23 18:19:37 -0600

    configure app routes

---
+++ examen/src/Components/CarParts.jsx
@@ -0,0 +1,10 @@
+import "./CarParts.css";
+
+export default function CarParts() {
+	return (
+		<main>
+			<h1>Car Parts</h1>
+			<p>Explora las piezas disponibles para tu vehiculo.</p>
+		</main>
+	);
+}
---
+++ examen/src/Components/Home.jsx
@@ -0,0 +1,10 @@
+import "./Home.css";
+
+export default function Home() {
+	return (
+		<main>
+			<h1>Home</h1>
+			<p>Bienvenido al catalogo de repuestos.</p>
+		</main>
+	);
+}
